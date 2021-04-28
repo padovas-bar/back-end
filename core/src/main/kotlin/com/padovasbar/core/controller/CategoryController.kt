77ef07bd73a55eb08779be0528077d6a0b37a77f
padovese <bpadovese@uolinc.com>
2021-04-27 23:47:13 -0300

    cross origin and docker compose

--- core/src/main/kotlin/com/padovasbar/core/controller/CategoryController.kt
+++ core/src/main/kotlin/com/padovasbar/core/controller/CategoryController.kt
@@ -2,6 +2,7 @@ package com.padovasbar.core.controller
 
 import com.padovasbar.core.model.Category
 import com.padovasbar.core.repository.CategoryRepository
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/category")
 class CategoryController(private val categoryRepository: CategoryRepository) {
 
+    @CrossOrigin
     @PostMapping
     fun create(@RequestBody category: Category){
         categoryRepository.save(category)
